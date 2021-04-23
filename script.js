7d24f6d9ecad2a097256639b7bff78de1c3e4e88
unknown <tsironiainafifaliana@gmail.com>
2021-04-23 14:26:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const quote = document.getElementById('quote');
+const pic =  document.getElementById('pic');
+
+const getQuote = () => {
+fetch('https://api.quotable.io/random')
+.then(res=> res.json())
+.then(data => {
+    quote.innerHTML = data.content + data.author
+});
+
+fetch('https://picsum.photos/1600/1000')
+.then((res) => { 
+    pic.innerHTML = `<img src=${res.url}>`
+});
+};
+
+
+quote.addEventListener('click',()=> getQuote());
